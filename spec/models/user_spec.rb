164ebb0092401d204629d7f642d7c9b3672769e1
Abhishek Ahirwar <abhiahirwar@gmail.com>
2013-04-13 14:43:46 +1000

    all test pass:

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,47 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe User do
+ before { @user = User.new(name: "Example User", email: "user@example.com")}
+
+ subject {@user}
+
+ it { should respond_to(:name)}
+ it { should respond_to(:email)}
+ it { should be_valid }
+
+	describe "when name is not present" do
+	 	before { @user.name = " " }
+	 	it { should_not be_valid }
+	end
+
+	describe "when email is not present" do
+		before { @user.email = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when name is too long" do
+		before { @user.name = "a" * 51 }
+		it { should_not be_valid }
+	end
+
+	describe "when email address is alreay taken" do
+		before do
+			user_with_same_eamil = @user.dup
+			user_with_same_eamil.email = @user.email.upcase
+			user_with_same_email.save
+		end
+
+		it { should_not be_valid }
+	end
+end
